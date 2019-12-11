17737d51bfa9dacc68a42a982d944153b6c1820f
lev2018 <43388464+lev2018@users.noreply.github.com>
2019-12-11 18:29:54 -0500

    Add files via upload

---
+++ myOnlineLibrary/inc/data.php
@@ -0,0 +1,169 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+ "title" => "The Lightning Thief",
+ "img" => "img/media/lightningThief.jpg",
+ "genre" => "Fantasy",
+ "format" => "Hardcover",
+ "year" => 2005,
+ "category" => "Books",
+ "authors" => [
+    "Rick Riordan"
+  ],
+  "publisher" => "Miramax Books, Puffin Books"
+];
+
+$catalog[102] = [
+  "title" => "The Great Gatsby",
+  "img" => "img/media/theGreatGatsby.jpg",
+  "genre" => "Tragedy",
+  "format" => "Hardcover",
+  "year" => 1925,
+  "category" => "Books",
+  "authors" => [
+    "F. Scott Fitzgerald"
+  ],
+  "publisher" => "Charles Scribner's Sons, Chatto and Windus"
+];
+
+$catalog[103] = [
+ "title" => "The Hound of Baskervilles",
+ "img" => "img/media/hound.jpg",
+ "genre" => "Detective fiction",
+ "format" => "Paperback",
+  "year" => 1902,
+  "category" => "Books",
+  "authors" => [
+    "Arthur Conan Doyle"
+  ],
+  "publisher" => "Charles Scribner's Sons, Chatto and Windus"
+];
+
+$catalog[104] = [
+ "title" => "Dracula",
+ "img" => "img/media/dracula.jpg",
+ "genre" => "Horror",
+ "format" => "Paperback",
+ "year" => 1897,
+ "category" => "Books",
+ "authors" => [
+    "Bram Stroker"
+  ],
+  "publisher" => "Archibald Constable and Company"
+];
+
+//Movies
+$catalog[201] = [
+"title" => "Paranormal Activity",
+"img" => "img/media/paranormalActivity.jpg",
+"genre" => "Supernatural",
+"format" => "DVD",
+"year" => 2007,
+"category" => "Movies",
+"director" => "Oren Peli",
+"writers" => [
+    "Oren Peli"
+],
+"stars" => [
+    "Katie Featherston",
+    "Micah Sloat"
+]
+];
+
+$catalog[202] = [
+"title" => "Ouija",
+"img" => "img/media/ouija.jpg",
+"genre" => "Horror",
+"format" => "DVD",
+"year" => 2014,
+"category" => "Movies",
+"director" => "Stiles White",
+"writers" => [
+    "Juliet Snowden, Stiles White"
+],
+"stars" => [
+    "Olivia Cooke",
+    "Daren Kagasoff",
+    "Douglas Smith",
+    "Bianca Santos"
+]
+];
+
+$catalog[203] = [
+"title" => "IT",
+"img" => "img/media/itPoster.jpg",
+"genre" => "Horror",
+"format" => "DVD",
+"year" => 2017,
+"category" => "Movies",
+"director" => "Andy Muschietti",
+"writers" => [
+    "Stephen King"
+],
+"stars" => [
+    "Jaeden Lieberher",
+    "Bill Skarsgard"
+]
+];
+
+$catalog[204] = [
+"title" => "Annabelle: Creation",
+"img" => "img/media/annabelleCreation.jpg",
+"genre" => "Supernatural",
+"format" => "DVD",
+"year" => 2017,
+"category" => "Movies",
+"director" => "David F. Sandberg",
+"writers" => [
+    "Gary Dauberman",
+],
+"stars" => [
+    "Stephanie Sigman",
+    "Talitha Bateman",
+    "Anthony LaPaglia",
+    "Miranda Otto"
+]
+];
+
+//Music
+$catalog[301] = [
+"title" => "K-12",
+"img" => "img/media/K-12.png",
+"genre" => "Electropop",
+"format" => "CD",
+"year" => 2019,
+"category" => "Music",
+"artist" => "Melanie Adele Martinez"
+];
+
+$catalog[302] = [
+"title" => "Trench",
+"img" => "img/media/trenchTOP.jpg",
+"genre" => "Alternative Rock",
+"format" => "CD",
+"year" => 2018,
+"category" => "Music",
+"artist" => "Twenty One Pilots"
+];
+
+$catalog[303] = [
+"title" => "Badlands",
+"img" => "img/media/halseyBadlands.png",
+"genre" => "Alternative pop",
+"format" => "CD",
+"year" => 2015,
+"category" => "Music",
+"artist" => "Halsey"
+];
+
+$catalog[304] = [
+"title" => "Origins",
+"img" => "img/media/originsID.png",
+"genre" => "Pop rock",
+"format" => "CD",
+"year" => 2018,
+"category" => "Music",
+"artist" => "Imagine Dragons"
+];
+?>
---
+++ myOnlineLibrary/inc/footer.php
@@ -0,0 +1,14 @@
+  </div>
+
+      <div class="footer">
+        <div class="wrapper">
+          <ul>
+            <li><a href="http://www.instagram.com/LaGuardiaCC/">Twitter</a></li>
+            <li><a href="http://www.facebook.com/LaGuardiaCC">Facebook</a></li>
+          </ul>
+          <p>&copy;<?php echo date("Y"); ?> My Online Library</p>
+
+        </div>
+      </div>
+  </body>
+  </html>
